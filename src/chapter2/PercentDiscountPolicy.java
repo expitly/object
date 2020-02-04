b6e686f180b5973e300c49cefdfcf469e21be50c
장진영 <jjy@jangjin-yeong-ui-MacBookPro.local>
2020-02-04 21:50:00 +0900

    chapter2

---
+++ src/chapter2/PercentDiscountPolicy.java
@@ -0,0 +1,17 @@
+package chapter2;
+
+public class PercentDiscountPolicy extends DiscountPolicy {
+    private double percent;
+
+    public PercentDiscountPolicy(double percent, DiscountCondition ... discountConditions) {
+        super((discountConditions));
+        this.percent = percent;
+    }
+
+    @Override
+    protected Money getDiscountAmount(Screening screening) {
+        return screening.getMovieFee().times(percent);
+
+
+    }
+}
